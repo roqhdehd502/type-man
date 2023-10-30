fe2892f65cba9227e11c831ee451e22b15322657
MinwooNa <natorque5552@gmail.com>
2023-10-30 21:44:47 +0900

    first commit

---
+++ enums/EnumWeeks.ts
@@ -0,0 +1,103 @@
+/**
+ * @enum {string}
+ */
+export const EnumWeekShortUpperEN = {
+  MON: "MON",
+  TUE: "TUE",
+  WED: "WED",
+  THU: "THU",
+  FRI: "FRI",
+  SAT: "SAT",
+  SUN: "SUN",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekShortLowerEN = {
+  MON: "mon",
+  TUE: "tue",
+  WED: "wed",
+  THU: "thu",
+  FRI: "fri",
+  SAT: "sat",
+  SUN: "sun",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekShortUpperBeginEN = {
+  MON: "Mon",
+  TUE: "Tue",
+  WED: "Wed",
+  THU: "Thu",
+  FRI: "Fri",
+  SAT: "Sat",
+  SUN: "Sun",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekFullUpperEN = {
+  MON: "MONDAY",
+  TUE: "TUESDAY",
+  WED: "WEDNESDAY",
+  THU: "THURSDAY",
+  FRI: "FRIDAY",
+  SAT: "SATURDAY",
+  SUN: "SUNDAY",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekFullLowerEN = {
+  MON: "monday",
+  TUE: "tuesday",
+  WED: "wednesday",
+  THU: "thursday",
+  FRI: "friday",
+  SAT: "saturday",
+  SUN: "sunday",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekFullUpperBeginEN = {
+  MON: "Monday",
+  TUE: "Tuesday",
+  WED: "Wednesday",
+  THU: "Thursday",
+  FRI: "Friday",
+  SAT: "Saturday",
+  SUN: "Sunday",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekShortKR = {
+  MON: "월",
+  TUE: "화",
+  WED: "수",
+  THU: "목",
+  FRI: "금",
+  SAT: "토",
+  SUN: "일",
+};
+
+/**
+ * @enum {string}
+ */
+export const EnumWeekFullKR = {
+  MON: "월요일",
+  TUE: "화요일",
+  WED: "수요일",
+  THU: "목요일",
+  FRI: "금요일",
+  SAT: "토요일",
+  SUN: "일요일",
+};
---
+++ types/numbers/TypeNumberCommons.ts
@@ -0,0 +1,9 @@
+export type TypeIndex = number;
+export type TypeFirstIndex = number;
+export type TypeLastIndex = number;
+export type TypeIndexOf = number;
+export type TypeZero = 0;
+export type TypeNegativeNumber = number;
+export type TypePositiveNumber = number;
+export type TypeMaximumNumber = number;
+export type TypeMinimumNumber = number;
